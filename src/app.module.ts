823890c895961041879cd4800b5bb2801bef5b11
111hunter <1621209357@qq.com>
2020-03-28 23:43:50 +0800

    pre

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,8 @@
-import { AuthController } from './auth/auth.controller';
-import { AppService } from './app.service';
-import { AppController } from './app.controller';
-import { UserService } from 'src/user/user.service';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { PhotoModule } from './photo/photo.module';
-import { UserEntity } from './user/user.entity';
 
 @Module({
   imports: [
